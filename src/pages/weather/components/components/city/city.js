dbd6768f956211f1db141b6c236b9aa903004145
navintc <navin.thamindu@gmail.com>
2023-03-12 08:10:31 +0530

    Updating the city card

--- src/pages/weather/components/components/city/city.js
+++ src/pages/weather/components/components/city/city.js
@@ -2,13 +2,15 @@ import './city.css'
 import {useState, useEffect} from 'react';
 import { getWeatherData } from '../../../../../api/client';
 import { Col, Container, Row } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner';
 
 
 const City = (props) => {
-    
-    const [cityWeather, setCityWeather] = useState([]);
+     //useState({}) used to make objects which is used for .maps
+    //useState([]) is an arrary. they will be not used for maps
+    const [cityWeather, setCityWeather] = useState(null);
+    const [value,setValue] = useState();
 
     // the city name came from the props will be used to find weather data
     useEffect(() => {    
@@ -18,7 +20,7 @@ const City = (props) => {
             // console.log(data)
         )
         .catch(error => console.error(error));
-        }, [props.name] //so it'll only be updated if there is change to props.name
+        }, [] //so it'll only be updated if there is change to props.name
         
     );
 
@@ -26,20 +28,45 @@ const City = (props) => {
     //worked properly or not by keeping an eye on to the cityWeather.
     useEffect(() => {
         console.log(cityWeather);
+        setValue("k");
       }, [cityWeather]);
 
     
+    // waits until the state is updated
+    if (!cityWeather) {
+    return <Spinner animation="grow" />;
+    }
 
     return(
-
         <Col sm={12} md={8} className='city-card'>
             <Card>
-                <Card.Header as="h5">{cityWeather.name}</Card.Header>
+                <Card.Header as="h3">{cityWeather.name}</Card.Header>
                 <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
-                    <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
-                    </Card.Text>
+                    
+                    {cityWeather.weather.map(detail => (
+                        <h2 className="w-main">Expect {detail.main}</h2>
+                    ))}
+
+                    <div className='w-info'>
+                        <div className='w-sec-1 col'>
+                            <Card.Text>
+                                Country: {cityWeather.sys.country}
+                                <br></br>
+                                Current Temperature is {Math.round(cityWeather.main.temp-273.15)} °C
+                            </Card.Text>
+                        </div>
+
+                        <div className='w-sec-1 col'>
+                            <Card.Text className="tech-data">
+                                Longtitude: {cityWeather.coord.lon} | Latitude: {cityWeather.coord.lat}                            
+                                <br></br>
+                                TimeZone: {cityWeather.timezone}
+                                <br></br>
+                                Pressure: {cityWeather.main.pressure}
+                            </Card.Text>
+                        </div>
+                    </div>
+
                 </Card.Body>
             </Card>
         </Col>
