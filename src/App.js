c7717a32f11ff51ee8ac1fbfd980e5a04c4aaa0f
navintc <navin.thamindu@gmail.com>
2023-03-14 13:01:54 +0530

    Removing Console logs and fixing crossorigin

--- src/App.js
+++ src/App.js
@@ -42,13 +42,13 @@ function App() {
 
       <Footer/>
 
-      <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossOrigin></script>
+      <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossOrigin="true"></script>
       <script
         src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
-        crossOrigin></script>
+        crossOrigin="true"></script>
       <script
         src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
-        crossOrigin></script>
+        crossOrigin="true"></script>
       <script>var Alert = ReactBootstrap.Alert;</script>
       
     </div>
